a1060de3b6e99024a88c236e076095fe4ce89b30
Guilherme Arsénio <guia994@gmail.com>
2022-10-28 16:21:03 +0100

    init new template

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,21 @@
+/**
+ * Vuetify3 Plugin
+ */
+// Vuetify
+import * as directives from 'vuetify/directives';
+import * as components from 'vuetify/components';
+import { createVuetify } from 'vuetify';
+
+// Misc
+import { loadFonts } from './webfontloader';
+loadFonts();
+
+// Styles
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
+
+export default createVuetify({
+  // https://next.vuetifyjs.com/en/getting-started/installation/
+  components,
+  directives,
+});
